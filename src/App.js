213d6df6644a319046b101e1a4cd6e0e2775a5eb
nallabothushailaja <nshailu2003@gmail.com>
2025-10-28 20:19:08 +0530

    Add React frontend source code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 Welcome to My React App</h1>
+      <p>This React app is running successfully on localhost!</p>
+    </div>
+  );
+}
+
+export default App;
+
